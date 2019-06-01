b9a767e7f3422307279b615f3231bf9fd2c63497
Cowboy8625 <cowboy8625@yahoo.com>
2019-06-01 16:35:29 -0500

    Added New Feature Boarders around Main Window/Frame

--- New_Main.py
+++ New_Main.py
@@ -9,6 +9,7 @@ try:
     from MyModules.inhousetest import pause
 except:
     pass
+
 class Point:
     '''
     Base Class
@@ -52,6 +53,12 @@ class Point:
         x = index % root.size.x
         y = int(index / root.size.x)
         return cls(x, y)
+
+    def x_same_as_y(self):
+        if self.x == self.y:
+            return True
+        else:
+            return False
 
 
 
@@ -149,12 +156,28 @@ class Grid:
     def grid_construct_grid(self):
         self._grid = ''.join([self.char * self.size.x + '\n'] * self.size.y)[0:-1]
 
-    def _parse(self):
+    def grid_parse(self):
         self._grid = [list(i) for i in list(self._grid.split('\n'))]
 
-    def _list_to_string(self):
+    def grid_list_to_string(self):
         self._grid = ''.join([i+'\n' for i in [''.join(i) for i in self._grid]])[0:-1]
 
+    def grid_get_index(self):
+        step = self.size.x
+        total = (self.size.x * self.size.y)
+        return self.__get_index_list(total, step)
+    
+
+    @staticmethod
+    def __get_index_list(total, step):
+        result = []
+        t = list(range(total))
+        for _ in range(total // step):
+            result.append(list(t[0:step]))
+            for _ in range(step):
+                t.pop(0)
+        return result
+    
 
 class Shape(Grid, ABC):
     '''
@@ -168,6 +191,18 @@ class Shape(Grid, ABC):
     ----------
         root: Frame
             takes frame object to place all Shapes on.
+
+        FROM GRID
+        ---------
+        cols: int = 5
+            aka how width the screen will be.
+        rows: int = 5
+            aka how tall/height the screen will be
+        char: chr, str = chr(9617)
+            aka what is the place holder.
+            can be a space ' '.
+
+        
     '''
     ELEMENTS = {
         'hBoarder': [chr(9552), chr(9472), chr(9473)],
@@ -176,20 +211,21 @@ class Shape(Grid, ABC):
         'rTee': [chr(9568), chr(9500), chr(9507)],
         'tTee': [chr(9574), chr(9516), chr(9523)],
         'bTee': [chr(9577), chr(9524), chr(9531)],
-        'ltConner': [chr(9556), chr(9581), chr(9487)],
-        'rtConner': [chr(9559), chr(9582), chr(9491)],
-        'lbConner': [chr(9562), chr(9584), chr(9495)],
-        'rbConner': [chr(9565), chr(9583), chr(9499)],
+        'ltCorner': [chr(9556), chr(9581), chr(9487)],
+        'rtCorner': [chr(9559), chr(9582), chr(9491)],
+        'lbCorner': [chr(9562), chr(9584), chr(9495)],
+        'rbCorner': [chr(9565), chr(9583), chr(9499)],
         'lSlope': ['/', '/', '/'],
         'rSlope': ['\\', '\\', '\\'],
         'cross': [chr(9580), chr(9532), chr(9547)]
         }
 
-    def __init__(self, root=None, *args, **kwargs):
+    def __init__(self, root=None, style=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.master = root
+        self.root = root
         self.shape_points = None
         self.start_point = Point()
+        self.style = style
 
     @abstractmethod
     def _draw(self):
@@ -214,7 +250,7 @@ class Shape(Grid, ABC):
         first_row = [i for i in range(starting_cell, (ending_cell + 1))]
         result.append(first_row)
         for _ in range(self.size.y - 1):
-            result.append(self._add_to_list(result[-1], self.master.size.x))
+            result.append(self._add_to_list(result[-1], self.root.size.x))
         self.index_list = result
 
     @staticmethod
@@ -225,7 +261,23 @@ class Shape(Grid, ABC):
 
 class Square(Shape):
     '''
-    Draws a Square on window
+    Description:
+    -----------
+        Draws a Square in a Frame.
+
+    Parameters
+    ----------
+        root: Frame
+            takes frame object to place all Shapes on.
+        FROM GRID
+        ---------
+        cols: int = 5
+            aka how width the screen will be.
+        rows: int = 5
+            aka how tall/height the screen will be
+        char: chr, str = chr(9617)
+            aka what is the place holder.
+            can be a space ' '.
     '''
 
     def __init__(self, *args, **kwargs):
@@ -237,18 +289,6 @@ class Square(Shape):
     def place_at(self):
         pass
 
-    def __get_outline(self):
-        top_horizontal_line = self.index_list[0]
-        leftside_vertical_line = None
-        rightside_vertical_line = None
-        bottom_horizontal_line = self.index_list[-1]
-
-        for idx in top_horizontal_line:
-            line_point = Point().fromIndex(self.root, idx)
-            self.root._grid[line_point.y][line_point.x] = self.ELEMENTS['hBoarder'][0]
-            del line_point
-
-
 
 class Rectangle(Shape):
 
@@ -256,9 +296,75 @@ class Rectangle(Shape):
         super().__init__(*args, **kwargs)
 
 
-class Boarder(Square, Rectangle):
+class Boarder(Shape):
+    '''
+    Boarder is a Base class to handle construction of 
+    boarder lines around the widget class.
+    
+    Square, Rectangle have a Composition relationship with 
+    the Boarder class.
+    '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.__get_outline()
+
+    def _draw(self):
+        pass
+
+    def place_at(self):
+        pass
+
+    def __get_outline(self):
+        
+        self.root.grid_parse()
+        
+        index_list = self.root.grid_get_index()
+
+        top_horizontal_line = index_list[0]
+        bottom_horizontal_line = index_list[-1]
+        leftside_vertical_line = self.__get_vertical_index_list(index_list, 0)
+        rightside_vertical_line = self.__get_vertical_index_list(index_list, -1)
+
+        for idx in top_horizontal_line:
+            line_point = Point().fromIndex(self.root, idx)
+            self.root._grid[line_point.y][line_point.x] = self.ELEMENTS['hBoarder'][0]
+            del line_point
+
+
+        for idx in bottom_horizontal_line:
+            line_point = Point().fromIndex(self.root, idx)
+            self.root._grid[line_point.y][line_point.x] = self.ELEMENTS['hBoarder'][0]
+            del line_point
+
+        for idx in leftside_vertical_line:
+            line_point = Point().fromIndex(self.root, idx)
+            self.root._grid[line_point.y][line_point.x] = self.ELEMENTS['vBoarder'][0]
+            del line_point
+
+        for idx in rightside_vertical_line:
+            line_point = Point().fromIndex(self.root, idx)
+            self.root._grid[line_point.y][line_point.x] = self.ELEMENTS['vBoarder'][0]
+            del line_point
+
+        left_top_corner = Point().fromIndex(self.root, top_horizontal_line[0])
+        self.root._grid[left_top_corner.y][left_top_corner.x] = self.ELEMENTS['ltCorner'][0]
+        right_top_corner = Point().fromIndex(self.root, top_horizontal_line[-1])
+        self.root._grid[right_top_corner.y][right_top_corner.x] = self.ELEMENTS['rtCorner'][0]
+        left_bottom_corner = Point().fromIndex(self.root, bottom_horizontal_line[0])
+        self.root._grid[left_bottom_corner.y][left_bottom_corner.x] = self.ELEMENTS['lbCorner'][0]
+        right_bottom_corner = Point().fromIndex(self.root, bottom_horizontal_line[-1])
+        self.root._grid[right_bottom_corner.y][right_bottom_corner.x] = self.ELEMENTS['rbCorner'][0]
+        
+        
+        del index_list
+        
+        self.root.grid_list_to_string()
+
+
+    @staticmethod
+    def __get_vertical_index_list(grid, index):
+        return [line[index] for line in grid]
+    
 
 
 class Widget(Grid, Window):
@@ -278,18 +384,18 @@ class Label(Widget):
 
     Parameters
     ----------
-        root: Frame Default : None
+        root: Frame = None
             takes Frame object as master/root
 
-        cols: int Default : 5
+        cols: int = 5
             aka how width the screen will be.
 
-        rows: int Default : 5
+        rows: int = 5
             aka how tall/height the screen will be
         
-        char: chr, string, int Default : chr(9608)
+        char: chr, string, int = chr(9608)
             aka what is the place holder.
-            can be nothing.
+            can be a space ' '.
     '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -300,16 +406,16 @@ class MessageBox(Widget):
 
     Parameters
     ----------
-        root: Frame Default : None
+        root: Frame = None
             takes Frame object as master/root
 
-        cols: int Default : 10
+        cols: int = 10
             aka how width the screen will be.
 
-        rows: int Default : 5
+        rows: int = 5
             aka how tall/height the screen will be
         
-        char: chr, string, int Default : chr(9608)
+        char: chr, string, int = chr(9608)
             aka what is the place holder.
             can be nothing.
     '''
@@ -317,7 +423,7 @@ class MessageBox(Widget):
         super().__init__(*args, **kwargs)
 
 
-class Frame(Widget, Boarder):
+class Frame(Widget):
     '''
     Master window class
 
@@ -325,22 +431,26 @@ class Frame(Widget, Boarder):
     
     Parameters
     ----------
-        root: Frame Default : None
+        style : int = 0
+            3 styles to choose from.
+        root: Frame = None
             takes frame object only if Frame is a Child.
 
-        cols: int Default : 5
+        cols: int = 5
             aka how width the screen will be.
 
-        rows: int Default : 5
+        rows: int = 5
             aka how tall/height the screen will be
         
-        char: chr, string, int Default : chr(9608)
+        char: chr, string, int = chr(9608)
             aka what is the place holder.
             can be nothing.
     '''
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, style=0, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.boarder = Boarder(self).pack()
+
 
 
 ##-- User interface --##
@@ -350,7 +460,7 @@ class App(Frame):
     How you would use the API
     '''
     def __init__(self):
-        self.master = Frame(cols=100, rows=30, char=chr(9608))
+        self.master = Frame(cols=100, rows=40, char=chr(9608))
         self.clear()
         self.initTUI()
 
@@ -362,12 +472,9 @@ class App(Frame):
 
 
 def test():
-    # Ignore Just for Testing.
-    root = Frame(cols=10, rows=10)
-    p1 = Point.fromIndex(root, 15)
-    print(p1)
-
+    root = Frame()
+    root.pack()
 if __name__ == "__main__":
-    app = App() 
+    app = App()
     import doctest
     doctest.testmod()
