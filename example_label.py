ebbecf94e5bb72376e9e639b7734dcb797cbb333
cowboy8625 <cowboy8625@yahoo.com>
2019-06-24 18:27:30 -0500

    refactored Shape classes, Boarder class, added TextPoint, GridPoint and Token class.  Added Label class logic

---
+++ example_label.py
@@ -0,0 +1,18 @@
+from TUI import Frame, Label, Tui
+
+
+class App(Frame):
+    def __init__(self, root):
+        self.root = root
+        self.initUI()
+
+    def initUI(self):
+        self.root.window_geometry(cols=100, rows=30)
+        self.label = Label(self.root, "Hello World", anchor=(1, 1), cols=20, rows=4)
+        self.label.pack()
+        self.root.mainloop()
+
+
+if __name__ == "__main__":
+    root = Tui()
+    app = App(root)
